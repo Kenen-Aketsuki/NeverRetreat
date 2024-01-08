5b4df518e3a4448ebf714cb2ee48945acc2624f5
轩辕明月 <1456287267@qq.com>
2024-01-08 16:33:01 +0800

    添加了棋子的存储表

--- Assets/Src/DataRWSD/FixSystemData.cs
+++ Assets/Src/DataRWSD/FixSystemData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml;
 
 public static class FixSystemData
 {
@@ -12,11 +13,18 @@ public static class FixSystemData
     public static string pieceDirectory;
     public static string TerrainDirectory;
 
+    //地形表
     public static Dictionary<string, BasicLandShape> GlobalBasicTerrainList = new Dictionary<string, BasicLandShape>();
+    //设施表
     public static Dictionary<string, Facility> GlobalFacilityList = new Dictionary<string, Facility>();
+    //特殊地形表
     public static Dictionary<string, Facility> GlobalSpecialTerrainList = new Dictionary<string, Facility>();
-
-
+    
+    //兵种表
+    public static Dictionary<string, XmlNode> GlobalPieceDataList = new Dictionary<string, XmlNode>();
+    //编制表
+    public static Dictionary <string, XmlNode> HumanOrganizationList = new Dictionary<string, XmlNode>();//人类方
+    public static Dictionary<string, XmlNode> ModeOrganizationList = new Dictionary<string, XmlNode>();//模组方
 
     public static void InitPath()
     {
