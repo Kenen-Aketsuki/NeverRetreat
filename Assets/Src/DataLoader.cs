c97f666a95a0b4d105f4c97202d26c3b9f7407b4
轩辕明月 <1456287267@qq.com>
2024-01-14 12:42:16 +0800

    补全部分地图的美术资源

--- Assets/Src/DataLoader.cs
+++ Assets/Src/DataLoader.cs
@@ -16,23 +16,6 @@ public class DataLoader : MonoBehaviour
     {
         BasicUtility.DataInit();//紗墮方象
 
-        /*Debug.LogError("！！！！！！");
-       foreach(KeyValuePair<string,XmlNode> land in FixSystemData.GlobalPieceDataList)
-       {
-           Debug.Log(land.Key + " ！ "+ land.Value.SelectSingleNode("name").InnerText);
-       }
-       Debug.LogError("！！！！！！");
-       foreach (KeyValuePair<string, XmlNode> land in FixSystemData.HumanOrganizationList)
-       {
-           Debug.Log(land.Key + " ！ " + land.Value.Attributes["name"].Value);
-       }
-       Debug.LogError("！！！！！！");
-       foreach (KeyValuePair<string, XmlNode> land in FixSystemData.CrashOrganizationList)
-       {
-           Debug.Log(land.Key + " ！ " + land.Value.Attributes["name"].Value);
-       }
-       */
-
         //霞編
         /*
         int i = 0, j = 0;
