ef30b78d84f9b8b78f811ccd6c87a663c310b5b6
轩辕明月 <1456287267@qq.com>
2024-01-23 12:49:20 +0800

    完成了读取地图相关的代码

--- Assets/Src/GameUtility.cs
+++ Assets/Src/GameUtility.cs
@@ -8,17 +8,22 @@ public static class GameUtility
 {
     public static Vector2Int mapSize;
 
-    public static void LoadMapFromSave(string path)//传入存档所在位置
-    {
-
-    }
-    public static void 从预设中读取地图()//字面意思
+    public static void 从预设中读取地图(bool fromSave, string Save)//字面意思
     {
         
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.Load(FixSystemData.GameInitDirectory + "\\Terrain.xml");
         XmlNode TerrainRoot = xmlDoc.DocumentElement;
-        xmlDoc.Load(FixSystemData.GameInitDirectory + "\\Facility.xml");
+
+        if (fromSave)
+        {
+            xmlDoc.Load(FixSystemData.GameInitDirectory + "\\" + Save + "\\Facility.xml");
+        }
+        else
+        {
+            xmlDoc.Load(FixSystemData.GameInitDirectory + "\\Facility.xml");
+        }
+        
         XmlNodeList FacilityColum = xmlDoc.DocumentElement.LastChild.ChildNodes;
         XmlNodeList FacilityRow = FacilityColum[0].ChildNodes;
 
@@ -107,7 +112,46 @@ public static class GameUtility
                 {
                     //进入代表此节点匹配此位置
                     //放置设施(格子内)
-
+                    if (row.SelectSingleNode("FacilityC") != null)
+                    {
+                        tileName = row.SelectSingleNode("FacilityC").InnerText;
+                        if (FixSystemData.GlobalFacilityList.ContainsKey(tileName))
+                        {
+                            FixGameData.FGD.MapList[7].SetTile(
+                                FixGameData.MapToWorld(columNo, rowNo),
+                                FixSystemData.GlobalFacilityList[tileName].Top);
+                        }
+                        else
+                        {
+                            FixGameData.FGD.MapList[7].SetTile(
+                                FixGameData.MapToWorld(columNo, rowNo),
+                                FixSystemData.GlobalSpFacilityList[tileName].Close);
+                        }
+                        
+                    }
+                    //放置设施（格子边）
+                    if (row.SelectSingleNode("river") != null)
+                    {
+                        sideSplit = row.SelectSingleNode("river").InnerText.Split("-");
+                        if (sideSplit[0] == "1")
+                        {
+                            FixGameData.FGD.MapList[1].SetTile(
+                                FixGameData.MapToWorld(columNo, rowNo),
+                                FixSystemData.GlobalBasicTerrainList["River"].Left);
+                        }
+                        if (sideSplit[1] == "1")
+                        {
+                            FixGameData.FGD.MapList[2].SetTile(
+                                FixGameData.MapToWorld(columNo, rowNo),
+                                FixSystemData.GlobalBasicTerrainList["River"].Top);
+                        }
+                        if (sideSplit[2] == "1")
+                        {
+                            FixGameData.FGD.MapList[3].SetTile(
+                                FixGameData.MapToWorld(columNo, rowNo),
+                                FixSystemData.GlobalBasicTerrainList["River"].Right);
+                        }
+                    }
 
                     //前进到下一行
                     rowFac++;
