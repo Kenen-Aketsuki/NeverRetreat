c7d69d106da3cadc848ac9a1651044248c3518f4
轩辕明月 <1456287267@qq.com>
2024-01-27 12:45:10 +0800

    修改了调试代码，无伤大雅

--- Assets/Src/UISrc/TestUI.cs
+++ Assets/Src/UISrc/TestUI.cs
@@ -13,19 +13,19 @@ public class TestUI : MonoBehaviour
         //    true);
         //Debug.Log("增加了一个棋子");
 
-        FixGameData.FGD.CrashPieceParent.GetComponent<PiecePool>().UpdateChildList("1\\Crash.Blade", FixGameData.MapToWorld(35, 3), new Vector3Int(0, 0, 0));
-        FixGameData.FGD.CrashPieceParent.GetComponent<PiecePool>().getChildByID("1\\Crash.Blade").transform.position = new Vector3Int(0, 0, 0);
+        FixGameData.FGD.HumanPieceParent.GetComponent<PiecePool>().UpdateChildList("1\\DawIII.101", FixGameData.MapToWorld(35, 5), new Vector3Int(0, 0, 0));
+        FixGameData.FGD.HumanPieceParent.GetComponent<PiecePool>().getChildByID("1\\DawIII.101").transform.position = new Vector3Int(0, 0, 0);
         Debug.Log("移动了一个棋子");
 
-        List<Tuple<string, int, int>> clist = FixGameData.FGD.CrashPieceParent.GetComponent<PiecePool>().childList;
-        Dictionary<int, Tuple<int, int>> listIndex = FixGameData.FGD.CrashPieceParent.GetComponent<PiecePool>().listIndex;
+        List<Tuple<string, int, int>> clist = FixGameData.FGD.HumanPieceParent.GetComponent<PiecePool>().childList;
+        Dictionary<int, Tuple<int, int>> listIndex = FixGameData.FGD.HumanPieceParent.GetComponent<PiecePool>().listIndex;
         Debug.Log(clist.Count);
         foreach (KeyValuePair<int, Tuple<int, int>> piece in listIndex)
         {
-            Debug.Log((piece.Key + 21) + "的长度为: " + piece.Value.Item2 + "起始坐标为: " + piece.Value.Item1);
+            Debug.Log(piece.Key + "的长度为: " + piece.Value.Item2 + "起始坐标为: " + piece.Value.Item1);
         }
 
-        List<GameObject> lst = FixGameData.FGD.CrashPieceParent.GetComponent<PiecePool>().getChildByPos(new Vector3Int(0, 0, 0));
+        List<GameObject> lst = FixGameData.FGD.HumanPieceParent.GetComponent<PiecePool>().getChildByPos(new Vector3Int(0, 0, 0));
         if (lst.Count != 0)
         {
             Debug.Log("在该处找到棋子,其名为：" + lst[0].name);
